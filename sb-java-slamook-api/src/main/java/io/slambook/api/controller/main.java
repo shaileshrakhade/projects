8aead729367cab45c9cb6acc0ebceada8cac0047
Shailesh Rakhade <shailesh15.sr@gmail.com>
2025-01-01 13:35:52 +0530

    start

---
+++ sb-java-slamook-api/src/main/java/io/slambook/api/controller/main.java
@@ -0,0 +1,5 @@
+package io.slambook.api.controller;
+
+public class main {
+
+}
---
+++ sb-java-slamook-api/src/test/java/io/slambook/api/SlamookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.slambook.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlamookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
